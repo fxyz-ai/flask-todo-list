20fb872bf05ddc8bd8cc86ae23be5b65caf49032
Mohamed Faiz <120184609+fxyz-ai@users.noreply.github.com>
2025-07-07 23:01:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,8 @@ def update(id):
         return render_template('update.html', task=task)
 
 
+
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
